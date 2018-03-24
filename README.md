5ecdc3d1b6e1dff531283d82cb324f1e4e98ba0e
instalator <vvvalt@mail.ru>
2018-03-24 13:10:22 +0700

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+![Logo](admin/mclighting.png)
+# ioBroker McLighting adapter
+=================
+
+[![NPM version](http://img.shields.io/npm/v/iobroker.mclighting.svg)](https://www.npmjs.com/package/iobroker.mclighting)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.mclighting.svg)](https://www.npmjs.com/package/iobroker.mclighting)
+[![Tests](http://img.shields.io/travis/instalator/ioBroker.mclighting/master.svg)](https://travis-ci.org/instalator/ioBroker.mclighting)
+
+[![NPM](https://nodei.co/npm/iobroker.mclighting.png?downloads=true)](https://nodei.co/npm/iobroker.mclighting/)
+
+## Description
+The driver
+
+## Описание
+Драйвер позволяет
+
+## Changelog
+
+### 0.0.1 (2018-03-24)
+* (instalator) initial
---
+++ main.js
@@ -0,0 +1,86 @@
+'use strict';
+var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+var adapter = new utils.Adapter('mclighting');
+
+adapter.on('unload', function (callback) {
+    try {
+        adapter.log.info('cleaned everything up...');
+        callback();
+    } catch (e) {
+        callback();
+    }
+});
+
+adapter.on('objectChange', function (id, obj) {
+    // Warning, obj can be null if it was deleted
+    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+});
+
+// is called if a subscribed state changes
+adapter.on('stateChange', function (id, state) {
+    // Warning, state can be null if it was deleted
+    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+
+    // you can use the ack flag to detect if it is status (true) or command (false)
+    if (state && !state.ack) {
+        adapter.log.info('ack is not set!');
+    }
+});
+
+// Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
+adapter.on('message', function (obj) {
+    if (typeof obj === 'object' && obj.message) {
+        if (obj.command === 'send') {
+            // e.g. send email or pushover or whatever
+            console.log('send command');
+
+            // Send response in callback if required
+            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+        }
+    }
+});
+
+adapter.on('ready', function () {
+    main();
+});
+
+function main() {
+
+    adapter.log.info('config test1: '    + adapter.config.test1);
+    adapter.log.info('config test1: '    + adapter.config.test2);
+    adapter.log.info('config mySelect: ' + adapter.config.mySelect);
+
+    adapter.setObject('testVariable', {
+        type: 'state',
+        common: {
+            name: 'testVariable',
+            type: 'boolean',
+            role: 'indicator'
+        },
+        native: {}
+    });
+
+    // in this mclighting all states changes inside the adapters namespace are subscribed
+    adapter.subscribeStates('*');
+
+
+    // the variable testVariable is set to true as command (ack=false)
+    adapter.setState('testVariable', true);
+
+    // same thing, but the value is flagged "ack"
+    // ack should be always set to true if the value is received from or acknowledged from the target system
+    adapter.setState('testVariable', {val: true, ack: true});
+
+    // same thing, but the state is deleted after 30s (getState will return null afterwards)
+    adapter.setState('testVariable', {val: true, ack: true, expire: 30});
+
+    // examples for the checkPassword/checkGroup functions
+    adapter.checkPassword('admin', 'iobroker', function (res) {
+        console.log('check user admin pw ioboker: ' + res);
+    });
+
+    adapter.checkGroup('admin', 'admin', function (res) {
+        console.log('check group user admin group admin: ' + res);
+    });
+
+}
